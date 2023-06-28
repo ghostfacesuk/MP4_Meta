42fba24348234ac6ee6a9bd31be977ab23b181be
Steve Griff <ssg-sas@outlook.com>
2023-06-28 20:54:33 +0100

    included 'vsd' and 'free' to the output

--- mp4_meta.py
+++ mp4_meta.py
@@ -6,7 +6,7 @@ def extract_numbers_between(file_path, start_value, end_value, output_file_path)
         end_index = content.find(end_value)
 
         while start_index != -1 and end_index != -1:
-            number = content[start_index + len(start_value):end_index]
+            number = content[start_index:end_index + len(end_value)]
             extracted_numbers.append(number)
             start_index = content.find(start_value, end_index + len(end_value))
             end_index = content.find(end_value, start_index + len(start_value))
@@ -25,3 +25,4 @@ end_value = "66726565"
 output_file_path = 'extracted_numbers.txt'
 
 extract_numbers_between(file_path, start_value, end_value, output_file_path)
+
